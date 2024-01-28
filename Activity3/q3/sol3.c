927e2e0f34b6fd4dd8c7303db1b261bc3848c977
emthy0 <40608590+emthy0@users.noreply.github.com>
2024-01-28 22:19:38 +0700

    Update sol3.c

---
+++ Activity3/q3/sol3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h> // <- add this line to use wait
+
+main()
+{
+  int i;
+  int n;
+  pid_t childpid;
+  n = 4;
+  for (i = 0; i < n; ++i)
+  {
+    childpid = fork();
+    if (childpid == 0)
+      exit(0);
+    sleep(100);
+  }
+  printf("This is process %ld with parent %ld\n", (long)getpid(), (long)getppid());
+}
