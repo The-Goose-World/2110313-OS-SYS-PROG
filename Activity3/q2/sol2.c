4e1f67155835d20e70d7040887fcd6e9909551aa
TitorPs360 <TitorPs360Gamer@gmail.com>
2024-01-28 18:38:10 +0700

    feat: :sparkles: Act3 Q2

---
+++ Activity3/q2/sol2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h> // <- add this line to use wait
+
+int main()
+{
+  int i;
+  int n;
+  pid_t childpid;
+  n = 4;
+
+  for (i = 0; i < n; ++i)
+  {
+    childpid = fork();
+    if (childpid > 0)
+      break;
+  }
+
+  wait(0);
+  printf("This is child process %ld with parent %ld\n", (long)getpid(), (long)getppid());
+
+  return 0;
+}
